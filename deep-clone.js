97de53baae3df5c26f1fbbdd39872cd1f467ecd5
kodyrouse <kodyrouse2010@gmail.com>
2019-03-19 10:53:32 -0400

    deep-clone.js

---
+++ deep-clone.js
@@ -0,0 +1,22 @@
+function deepCloneObject(inputObject) {
+
+  // Prevents invalid input
+	if (typeof inputObject !== "object") return;
+
+	var newObject = {};
+
+	for (key in inputObject) {
+
+		if (!inputObject.hasOwnProperty(key)) continue;
+
+		const priorKey = key;
+    
+    // Either recursively deep clones nested object or copies object value for new key value
+		const newObjectVal = (typeof inputObject[priorKey] == "object") ? deepCloneObject(inputObject[priorKey]) : inputObject[priorKey];
+    
+    // Sets new value
+		newObject[priorKey] = newObjectVal;
+	}
+
+	return newObject;
+}
