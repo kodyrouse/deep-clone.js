4ac562937e515e6c9bec49244dc24ceb588c2c22
kodyrouse <kodyrouse2010@gmail.com>
2019-03-25 19:59:28 -0400

    Update deep-clone.js
    Fixed issue with it copying array values into a new object

--- deep-clone.js
+++ deep-clone.js
@@ -1,22 +1,64 @@
-function deepCloneObject(inputObject) {
+class DeepClone {
 
-  	// Prevents invalid input
-	if (typeof inputObject !== "object") return;
+	static clone(input) {
 
-	var newObject = {};
-
-	for (key in inputObject) {
-
-		if (!inputObject.hasOwnProperty(key)) continue;
-
-		const priorKey = key;
-    
-    		// Either recursively deep clones nested object or copies object value for new key value
-		const newObjectVal = (typeof inputObject[priorKey] == "object") ? deepCloneObject(inputObject[priorKey]) : inputObject[priorKey];
-    
-    		// Sets new value
-		newObject[priorKey] = newObjectVal;
+		// Prevents invalid input
+		if (!this._validateInputForDeepClone(input)) {
+			console.warn(`Deep Clone input invalid : ${input} is not of type object`);
+			return;
+		}
+ 
+		// Starts deep clone of object || array depending on input
+		// I use _validateArray first because typeof "object" passes true for an array
+		return (this._validateArray(input)) ? this._deepCloneArray(input) : this._deepCloneObject(input);
 	}
 
-	return newObject;
+	static _deepCloneObject(object) {
+
+		const newObject = {};
+		const objectKeys = Object.keys(object);
+
+		objectKeys.forEach(key => {
+
+			const priorVal = object[key];
+
+			// Either recursively deep clones nested object / array or copies object value for new key value
+			const newValue = this._copyValue(priorVal);
+			newObject[key] = newValue;
+		});
+
+		return newObject;
+	}
+
+	static _deepCloneArray(array) {
+
+		const newArray = [];
+
+		array.forEach(value => {
+			const newValue = this._copyValue(value, true);
+			newArray.push(newValue);
+		});
+
+		return newArray;
+	}
+
+	static _copyValue(priorVal) {
+
+		// I use _validateArray first because typeof "object" passes true for an array
+		return (this._validateArray(priorVal)) ? this._deepCloneArray(priorVal)
+			: (this._validateObject(priorVal)) ? this._deepCloneObject(priorVal)
+			: priorVal;
+	}
+
+	static _validateInputForDeepClone(input) {
+		return this._validateObject(input) || this._validateArray(input);
+	}
+
+	static _validateObject(input) {
+		return typeof input === "object";
+	}
+
+	static _validateArray(input) {
+		return Array.isArray(input);
+	}
 }
